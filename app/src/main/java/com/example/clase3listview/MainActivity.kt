1806d11964efe0480951f0b9260ef7144b661758
ThomasRomeu <losromeu@hotmail.com>
2023-04-25 21:37:58 -0300

    Se cambia 1 nombre

--- app/src/main/java/com/example/clase3listview/MainActivity.kt
+++ app/src/main/java/com/example/clase3listview/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         listView = findViewById(R.id.listView)
         button = findViewById(R.id.button)
 
-        val nombres = listOf("Pepe", "Lorena", "Luciano", "Tomas", "Carolina", "Juancito")
+        val nombres = listOf("Ludmila", "Lorena", "Luciano", "Tomas", "Carolina", "Juancito")
 
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, nombres)
